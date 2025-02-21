6d62810764a67157152c92705714a238d11008cf
yunsy1103 <yunsy1103@naver.com>
2025-02-21 12:47:22 +0900

    init: project 생성

---
+++ src/main/java/com/zero/jeosoknohwa/JeosoknohwaApplication.java
@@ -0,0 +1,13 @@
+package com.zero.jeosoknohwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeosoknohwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeosoknohwaApplication.class, args);
+	}
+
+}
